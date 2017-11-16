580a23c8bb5ca11810f32e16c710cf8e3688de95
touyou <fujiyou1121@gmail.com>
2017-11-16 10:55:48 +0900

    add Storyboard Instantiable

---
+++ XmasPresentGo/Protocols/StoryboardInstantiable.swift
@@ -0,0 +1,34 @@
+//
+//  StoryboardInstantiable.swift
+//  XmasPresentGo
+//
+//  Created by 藤井陽介 on 2017/11/15.
+//  Copyright © 2017年 touyou. All rights reserved.
+//
+
+import UIKit
+
+protocol StoryboardInstantiable: class {
+    
+    static var storyboardName: String { get }
+}
+
+extension StoryboardInstantiable where Self: UIViewController {
+    
+    static var storyboardName: String {
+        
+        return String(describing: self)
+    }
+    
+    static func instantiate() -> Self {
+        
+        let storyboard = UIStoryboard(name: storyboardName, bundle: nil)
+        
+        guard let controller = storyboard.instantiateInitialViewController() as? Self else {
+          
+            assert(false, "生成したいViewControllerと同じ名前のStorybaordが見つからないか、Initial ViewControllerに設定されていない可能性があります。")
+        }
+        
+        return controller
+    }
+}
--- XmasPresentGo/Scenes/MainViewController.swift
+++ XmasPresentGo/Scenes/MainViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class MainViewController: UIViewController {
 
     @IBOutlet var sceneView: ARSCNView!
     
@@ -51,17 +51,20 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
+}
 
-    // MARK: - ARSCNViewDelegate
+// MARK: - ARSCNViewDelegate
+
+extension MainViewController: ARSCNViewDelegate {
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
+    /*
+     // Override to create and configure nodes for anchors added to the view's session.
+     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+     let node = SCNNode()
      
-        return node
-    }
-*/
+     return node
+     }
+     */
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
@@ -78,3 +81,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
     }
 }
+
+// MARK: - Storyboard Instatiable
+
+extension MainViewController: StoryboardInstantiable {}
