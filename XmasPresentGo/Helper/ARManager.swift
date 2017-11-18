08977d8e16156d35f2d306a1dfaae334e9fce275
touyou <fujiyou1121@gmail.com>
2017-11-18 10:50:01 +0900

    add 3D Model Datas

---
+++ XmasPresentGo/Helper/ARManager.swift
@@ -0,0 +1,18 @@
+//
+//  ARManager.swift
+//  XmasPresentGo
+//
+//  Created by 藤井陽介 on 2017/11/17.
+//  Copyright © 2017年 touyou. All rights reserved.
+//
+
+import SceneKit
+import ARKit
+
+public class ARManager: NSObject {
+    
+    static let shared = ARManager()
+}
+
+extension ARManager {
+}
