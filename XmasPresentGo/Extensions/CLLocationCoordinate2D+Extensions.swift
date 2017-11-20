2b8964d28b3d63ab4b75f5ec8723342284209068
touyou <fujiyou1121@gmail.com>
2017-11-20 10:18:51 +0900

    add some location utility method

--- XmasPresentGo/Extensions/CLLocationCoordinate2D+Extensions.swift
+++ XmasPresentGo/Extensions/CLLocationCoordinate2D+Extensions.swift
@@ -9,8 +9,12 @@
 import Foundation
 import CoreLocation
 
+let metersPerRadianLat: Double = 6373000.0
+let metersPerRadianLon: Double = 5602900.0
+
 extension CLLocationCoordinate2D {
     
+    /// Calcurate bearing toward other coordinate
     func bearing(to coordinate: CLLocationCoordinate2D) -> Double {
         
         let a = sin(coordinate.longitude.radian - longitude.radian) * cos(coordinate.latitude.radian)
@@ -18,8 +22,39 @@ extension CLLocationCoordinate2D {
         return atan2(a, b)
     }
     
+    /// Calcurate direction toward other coordinate
     func direction(to coordinate: CLLocationCoordinate2D) -> CLLocationDirection {
         
         return self.bearing(to: coordinate).degree
+    }
+    
+    /// Calcurate coordinate with bearing and distance
+    func coordinate(with bearing: Double, and distance: Double) -> CLLocationCoordinate2D {
+        
+        let distRadiansLat = distance / metersPerRadianLat
+        let distRadiansLon = distance / metersPerRadianLon
+        
+        let lat1 = self.latitude.radian
+        let lon1 = self.longitude.radian
+        
+        let lat2 = asin(sin(lat1) * cos(distRadiansLat) + cos(lat1) * sin(distRadiansLat) * cos(bearing))
+        let lon2 = lon1 + atan2(sin(bearing) * sin(distRadiansLon) * cos(lat1), cos(distRadiansLon) - sin(lat1) * sin(lat2))
+        
+        return CLLocationCoordinate2D(latitude: lat2.degree, longitude: lon2.degree)
+    }
+}
+
+extension Double {
+    
+    /// Convert degree value to radian value
+    var radian: Double {
+        
+        return self * .pi / 180.0
+    }
+    
+    /// Convert radian value to degree value
+    var degree: Double {
+        
+        return self * 180.0 / .pi
     }
 }
--- XmasPresentGo/Helper/ARHelperKit.swift
+++ XmasPresentGo/Helper/ARHelperKit.swift
@@ -9,6 +9,7 @@
 import SceneKit
 import ARKit
 import CoreLocation
+import GLKit.GLKMatrix4
 
 /// SCNModelGeneratable Protocol
 ///
@@ -138,6 +139,45 @@ extension ARCLManager: CLLocationManagerDelegate {
         _coordinate = newLocation.coordinate
         _longitude = newLocation.coordinate.longitude
         _latitude = newLocation.coordinate.latitude
+    }
+}
+
+// MARK: - Matrix Helper
+
+/// Matrix calcuration helpers
+/// matrix_float4x4 is
+/// column 0 to 3 and row is x, y, z, w
+class MatrixHelper {
+    
+    static func rotateAroundY(with matrix: matrix_float4x4, for degrees: Float) -> matrix_float4x4 {
+        
+        var matrix: matrix_float4x4 = matrix
+        
+        matrix.columns.0.x = cos(degrees)
+        matrix.columns.0.z = -sin(degrees)
+        
+        matrix.columns.2.x = sin(degrees)
+        matrix.columns.2.z = cos(degrees)
+        return matrix.inverse
+    }
+    
+    static func translationMatrix(with matrix: matrix_float4x4, for translation: vector_float4) -> matrix_float4x4 {
+        
+        var matrix = matrix
+        matrix.columns.3 = translation
+        return matrix
+    }
+    
+    /// This method calcurate distance and bearing between one and the other location
+    static func transformMatrix(for matrix: simd_float4x4, originLocation: CLLocation, location: CLLocation) -> simd_float4x4 {
+        
+        let distance = Float(location.distance(from: originLocation))
+        let bearing = GLKMathDegreesToRadians(Float(originLocation.coordinate.direction(to: location.coordinate)))
+        let position = vector_float4(0.0, 0.0, -distance, 0.0)
+        let translationMatrix = MatrixHelper.translationMatrix(with: matrix_identity_float4x4, for: position)
+        let rotationMatrix = MatrixHelper.rotateAroundY(with: matrix_identity_float4x4, for: bearing)
+        let transformMatrix = simd_mul(rotationMatrix, translationMatrix)
+        return simd_mul(matrix, transformMatrix)
     }
 }
 
